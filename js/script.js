c8c68af3923ed8c1c5db57f42780da6b43c3c074
Willian Oliveira <williancontato67@gmail.com>
2023-11-16 22:37:05 -0300

    refatoração com class do scrollAnima

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 import ScrollSuave from './modules/scroll-suave.js';
-import initAnimacaoScroll from './modules/scroll-animacao.js';
+import ScrollAnima from './modules/scrollAnima.js';
 import Accordion from './modules/accordion.js';
 import TabNav from './modules/tabnav.js';
 import Modal from './modules/modal.js';
@@ -15,13 +15,14 @@ const accordion = new Accordion('[data-anime="accordion"] dt');
 const tabNav = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section');
 const modal = new Modal('[data-modal="container"]', '[data-modal="abrir"]', '[data-modal="fechar"]');
 const tooltip = new Tooltip('[data-tooltip]');
+const scrollAnima = new ScrollAnima('[data-anime="scroll"]');
+scrollAnima.init();
 scrollSuave.init();
 accordion.init();
 tabNav.init();
 modal.init();
 tooltip.init();
 
-initAnimacaoScroll();
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
